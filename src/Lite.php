cbb3c5f3f95a7902b0a72786a77495ada0c006b7
dogstar <chanzonghuang@gmail.com>
2017-12-29 20:03:03 +0800

    bugifxed

--- src/Lite.php
+++ src/Lite.php
@@ -8,23 +8,11 @@ namespace PhalApi\QrCode;
  * @author: dogstar 2017-11-21
  */
 
+require_once dirname(__FILE__) . '/phpqrcode/qrlib.php';
+
 class Lite {
 
-    protected $config;
-
-    protected $client;
-
-    /**
-     * @param string $config['accessKey']  统一的key
-     * @param string $config['secretKey']
-     * @param string $config['space_bucket']  自定义配置的空间
-     * @param string $config['space_host']  
-     */
-    public function __construct($config = NULL) {
-        $this->config = $config;
-
-        if ($this->config === NULL) {
-            $this->config = \PhalApi\DI()->config->get('app.QrCode');
-        }
-    }
+    public function png($text, $outfile = false, $level = 'L', $size = 3, $margin = 4, $saveandprint = false) {
+        return \QRcode::png($text, $outfile, $level, $size, $margin, $saveandprint);
+    } 
 }
